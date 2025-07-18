76f9e17294ff3ea61e439491e7418c45fa464012
Matteo Chiti <matteo@we-rad.com>
2025-07-18 18:48:32 +0200

    Improved theme dark mode switcher.

--- components/ThemeToggle.vue
+++ components/ThemeToggle.vue
@@ -1,7 +1,7 @@
 <template>
   <div :class="[theme === 'dark' ? 'dark-state' : 'light-state', 'flex gap-2 items-center']">
-    <button @click="toggleTheme" class="text-xs text-gray-400 border border-black dark:border-white rounded-full inline-block w-8 h-5 p-[1px] text-left dark:text-right duration-300 ease-in-out">
-      <span class="inline-block w-4 h-4 rounded-full bg-black dark:bg-white duration-300 ease-in-out relative hover:opacity-70"></span>
+    <button @click="toggleTheme" class="group text-xs text-gray-400 border border-black dark:border-white rounded-full inline-block w-8 h-5 p-[1px] text-left dark:text-right hover:bg-slate-200 hover:dark:bg-gray-700 duration-300 ease-in-out">
+      <span class="inline-block w-4 h-4 rounded-full bg-black dark:bg-white duration-300 ease-in-out relative group-hover:opacity-90"></span>
     </button>
   </div>
 </template>
